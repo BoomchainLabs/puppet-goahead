0405fd5db145d4171374b097a4eafb24d993ac22
Andreas Paul <xorpaul@gmail.com>
2019-12-02 13:19:35 +0100

    add manage_os_restart_hooks_dir parameter

--- manifests/init.pp
+++ manifests/init.pp
@@ -40,6 +40,7 @@ class goahead (
   String $restart_condition_script = "${config_directory}/check_restart_condition.sh",
   Integer $restart_condition_script_exit_code_for_reboot = 0,
   String $os_restart_hooks_dir = "${config_directory}/restart_hooks.d",
+  Boolean $manage_os_restart_hooks_dir = true,
 ){
 
   if $add_goahead_user {
@@ -83,6 +84,16 @@ class goahead (
         owner   => 'root',
         group   => 'root',
         mode    => '0440',
+      }
+    }
+
+    if $manage_os_restart_hooks_dir {
+      file { "${os_restart_hooks_dir}/":
+        ensure  => directory,
+        owner   => $goahead_user,
+        group   => 'root',
+        mode    => '0550',
+        purge   => true,
       }
     }
 
